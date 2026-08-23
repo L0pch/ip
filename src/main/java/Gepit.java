e780a18e5954bb6ed225a8d643e21a969229e946
Fredrick CC <cfredc2004@gmail.com>
2026-08-23 16:31:53 +0800

    Completed Level-1 task
    Added input and repsonse sequence

--- src/main/java/Gepit.java
+++ src/main/java/Gepit.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Gepit {
     private static final String INTRO_MESSAGE = """
             ____________________________________________________________
@@ -15,6 +17,24 @@ public class Gepit {
 
     public static void main(String[] args) {
         System.out.println(INTRO_MESSAGE);
+        runChatBot();
         System.out.println(GOODBYE_MESSAGE);
+    }
+
+    private static void runChatBot() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                return;
+            }
+
+            String output = "____________________________________________________________\n" +
+                    input +
+                    "\n____________________________________________________________";
+
+            System.out.println(output);
+        }
     }
 }
