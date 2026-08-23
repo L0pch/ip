9430af9f1ed174fda9ef6232a8b3a1da8ce5f6a7
Fredrick CC <cfredc2004@gmail.com>
2026-08-23 15:57:04 +0800

    Complete level-0 chatbot set-up
    Chatbot Re-named, greeting and forewell messages updated

---
+++ src/main/java/Gepit.java
@@ -0,0 +1,20 @@
+public class Gepit {
+    private static final String INTRO_MESSAGE = """
+            ____________________________________________________________
+            Hey man I'm Gepit
+            What can I do for you?
+            ____________________________________________________________
+            """;
+
+    private static final String GOODBYE_MESSAGE = """
+            ____________________________________________________________
+            Bye Friendo
+            See you again next time :)
+            ____________________________________________________________
+            """;
+
+    public static void main(String[] args) {
+        System.out.println(INTRO_MESSAGE);
+        System.out.println(GOODBYE_MESSAGE);
+    }
+}
